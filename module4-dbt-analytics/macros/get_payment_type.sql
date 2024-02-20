c38e4f7807e4e09a04ef932ba54e9841a7f6ee9d
tasneem.ahmed@datain.sa <tasneem.ahmed@datain.sa>
2024-02-20 12:31:14 +0000

    Add  get_Payment_type_desc() macro

---
+++ module4-dbt-analytics/macros/get_payment_type.sql
@@ -0,0 +1,16 @@
+{#
+    This macro returns the description of the payment_type 
+#}
+
+{% macro get_payment_type_desc(payment_type) -%}
+    case {{payment_type}}
+        when '1.0' then 'Credit card'
+        when '2.0' then 'Cash'
+        when '3.0' then 'No charge'
+        when '4.0' then 'Dispute'
+        when '5.0' then 'Unknown'
+        when '6.0' then 'Voided trip'
+        else 'EMPTY'
+    end
+
+{%- endmacro %}
